21c872b73340e4f664e1541d7d60f595162ed2d1
Wallan Aquino <wallanaq.dev@gmail.com>
2023-05-29 16:47:33 -0300

    initial commit

---
+++ src/main/java/com/wallanaq/api/KeycloakIntrospectEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.wallanaq.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakIntrospectEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakIntrospectEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallanaq/api/KeycloakIntrospectEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallanaq.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakIntrospectEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
